74d3e5c96f65f62c41f28243a281ca8218f1bfba
Brian Feldman <brian.feldman42@gmail.com>
2020-08-24 14:20:31 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,13 +1,15 @@
 def addition(num1, num2)
-
+  sum = num1 + num2
+  sum
 end
 
 def subtraction(num1, num2)
-
+  diff = num1 - num2
+  diff
 end
 
 def division(num1, num2)
-
+  result = num1 / num2
 end
 
 def multiplication(num1, num2)
