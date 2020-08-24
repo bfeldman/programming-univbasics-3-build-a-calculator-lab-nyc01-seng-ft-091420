7906168a2b3e0bbc6cabbbc7f0b7a863c7f3b7ac
Brian Feldman <brian.feldman42@gmail.com>
2020-08-24 14:21:40 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,6 +1,5 @@
 def addition(num1, num2)
   sum = num1 + num2
-  sum
 end
 
 def subtraction(num1, num2)
@@ -10,14 +9,16 @@ end
 
 def division(num1, num2)
   result = num1 / num2
+  result
 end
 
 def multiplication(num1, num2)
-
+  product = num1 * num2
+  product
 end
 
 def modulo(num1, num2)
-
+  
 end
 
 def square_root(num)
